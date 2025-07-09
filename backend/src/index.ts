474d90d7f5913aef39db7f43f7c1e0126d5ca20c
rajyadav63 <vector36razz@gmail.com>
2025-07-09 15:19:30 +0530

    api added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,36 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+
+const port = 33000
 
 const app = express()
+
+app.post('/api/v1/signup/', (req, res) => {
+
+})
+app.post('/api/v1/signin/', (req, res) => {
+
+})
+app.post('/api/v1/content/', (req, res) => {
+
+})
+app.get('/api/v1/content/', (req, res) => {
+
+})
+app.delete('/api/v1/content/', (req, res) => {
+
+})
+app.delete('/api/v1/brain/share', (req, res) => {
+
+})
+app.get('/api/v1/brain/:shareLink', (req, res) => {
+
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running at port: ${port}`);
+
+})
