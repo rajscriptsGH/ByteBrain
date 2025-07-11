4b6302d45372e5b8655970e726822211bb554765
rajyadav63 <vector36razz@gmail.com>
2025-07-11 15:58:53 +0530

    content schema added

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -11,3 +11,13 @@ const UserSchema = new Schema({
 
 
 export const UserModel = model("User", UserSchema);
+
+
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags: [{ type: mongoose.Types.ObjectId, ref: 'Tag' }],
+    userId: [{ type: mongoose.Types.ObjectId, ref: 'User', required: true }],
+})
+
+export const ContentModel = model("Content", ContentSchema)
