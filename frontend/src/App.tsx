6964091a669c0f6536a9d7355a8f0e72856c89f5
rajyadav63 <vector36razz@gmail.com>
2025-07-14 12:42:24 +0530

    Card components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,30 @@ import './index.css'
 import { Button } from './components/Button';
 import { PlusIcon } from './icons/PlusIcon';
 import { ShareIcon } from './icons/ShareIcon';
+import { Card } from './components/Card';
 
 function App() {
 
 
   return (
+    <>
     <div className='flex'>
     <Button startIcon={<ShareIcon size={'lg'}/>}  variant='secondary' size='md' text='Share'></Button>
 
     <Button startIcon={<PlusIcon size='lg'/>} variant='primary' size='md' text='Add content'></Button>
     </div>
+
+    <div>
+      <Card/>
+    </div>
+
+
+
+
+
+
+
+    </>
   );
 }
 
---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,9 @@
+
+export const Card = () => {
+  return (
+    <div className="border border-slate-300 max-w-64 rounded-lg h-80 mx-5 my-5 py-3 px-5">
+      
+    </div>
+  )
+}
+
