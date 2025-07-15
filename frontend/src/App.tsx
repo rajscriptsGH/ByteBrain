bbcffa8f71243a929b2f374b6a169888184eb079
rajyadav63 <vector36razz@gmail.com>
2025-07-15 11:32:58 +0530

    card section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,15 +10,23 @@ function App() {
 
   return (
     <>
-    <div className='flex justify-end items-end mt-5 mr-10'>
-    <Button startIcon={<ShareIcon size={'lg'}/>}  variant='secondary' size='md' text='Share'></Button>
+      <div className='flex justify-end items-end mt-5 mr-10'>
+        <Button startIcon={<ShareIcon size={'lg'} />} variant='secondary' size='md' text='Share'></Button>
 
-    <Button startIcon={<PlusIcon size='lg'/>} variant='primary' size='md' text='Add content'></Button>
-    </div>
+        <Button startIcon={<PlusIcon size='lg' />} variant='primary' size='md' text='Add content'></Button>
+      </div>
 
-    <div className='flex justify-around items-center'>
-      <Card/>
-    </div>
+      <div className='flex justify-around items-center'>
+        <Card
+          title='Cracked US Startup'
+          link='https://www.youtube.com/watch?v=-bLHgQqkTHk'
+          type='youtube' />
+
+        <Card
+          title='My Intro on X'
+          link='https://x.com/rajscriptsX/status/1944876307403207116'
+          type='twitter' />
+      </div>
 
 
 
