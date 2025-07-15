7db0ff4040839fb3fcd229bf9e10317acf7fd617
rajyadav63 <vector36razz@gmail.com>
2025-07-15 12:57:30 +0530

    Card component done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,8 +9,8 @@ function App() {
 
 
   return (
-    <>
-      <div className='flex justify-end items-end mt-5 mr-10'>
+    <div className='h-screen'>
+      <div className='flex justify-end items-end mt-5 mr-10 h-auto'>
         <Button startIcon={<ShareIcon size={'lg'} />} variant='secondary' size='md' text='Share'></Button>
 
         <Button startIcon={<PlusIcon size='lg' />} variant='primary' size='md' text='Add content'></Button>
@@ -18,11 +18,19 @@ function App() {
 
       <div className='flex justify-around items-center flex-col sm:flex-row flex-wrap'>
         <Card
-          title='Cracked US Startup'
-          link='https://www.youtube.com/watch?v=-bLHgQqkTHk'
-          type='youtube'
-          detail='Video about 100xDev success story'
+          title='My Setup'
+          link='https://x.com/rajscriptsX/status/1942073859769073756'
+          type='twitter'
+          detail=''
         />
+
+        <Card
+          title='Notion ToDos'
+          link='https://www.notion.so/ToDos-App-d270cea6af3244248ac6cdd0129a87b6?source=copy_link'
+          type='note'
+          detail='Notion Docs of my TODos'
+        />
+
 
         <Card
           title='My Intro on X'
@@ -30,15 +38,17 @@ function App() {
           type='twitter'
           detail='Video about 100xDev success story'
         />
+
+        <Card
+          title='Cracked US Startup'
+          link='https://www.youtube.com/watch?v=-bLHgQqkTHk'
+          type='youtube'
+          detail='Video about 100xDev success story'
+        />
+
+
       </div>
-
-
-
-
-
-
-
-    </>
+    </div>
   );
 }
 
--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useRef } from "react";
 import { ShareIcon } from "../icons/ShareIcon";
+import { DeleteIcon } from "../icons/DeleteIcon";
+import { YouTubeIcon } from "../icons/YoutubeIcon";
+import { TwitterIcon } from "../icons/TwitterIcon";
+import { NoteIcon } from "../icons/NoteIcon";
+import { LinkIcon } from "../icons/LinkIcon";
+import { PlusIcon } from "../icons/PlusIcon";
 
 interface CardProps {
   title: string;
   link: string;
-  type: "twitter" | "youtube";
-  detail: string;
+  type: "twitter" | "youtube" | "note" | "links";
+  detail?: string;
 }
 
 function getYouTubeEmbedUrl(url: string): string {
@@ -31,6 +37,16 @@ function getYouTubeEmbedUrl(url: string): string {
 export const Card = ({ title, link, type, detail }: CardProps) => {
   const twitterRef = useRef<HTMLDivElement>(null);
 
+  const iconMap = {
+    youtube: <YouTubeIcon size="lg" />,
+    twitter: <TwitterIcon size="lg" />,
+    note: <NoteIcon size="lg" />,
+    links: <LinkIcon size="lg" />,
+    share: <ShareIcon size="lg" />,
+    delete: <DeleteIcon size="lg" />,
+    plus: <PlusIcon size="lg" />,
+  }
+
   useEffect(() => {
     if (type === "twitter" && window?.twttr?.widgets) {
       window.twttr.widgets.load(twitterRef.current);
@@ -41,22 +57,22 @@ export const Card = ({ title, link, type, detail }: CardProps) => {
   const normalizedLink = link.replace("x.com", "twitter.com");
 
   return (
-    <div className="flex justify-between flex-col  border border-slate-300 min-w-72 rounded-lg mt-10 mx-5 my-5 py-3 px-5 shadow-md">
+    <div className="flex justify-between flex-col  border border-slate-300 w-96 overflow-hidden rounded-lg mt-5 mx-5 my-5 py-3 px-5 shadow-md">
 
       {/* Header section */}
       <div className="flex justify-between">
         <div className="flex gap-3">
-          <ShareIcon size="lg" />
+          {iconMap[type]}
           <p className="font-semibold">{title}</p>
         </div>
         <div className="flex gap-3">
           <ShareIcon size="lg" />
-          <ShareIcon size="lg" />
+          <DeleteIcon size="lg" />
         </div>
       </div>
 
       {/* content section */}
-      <div className="w-full h-80 mt-5 mb-12">
+      <div className="w-full h-80 mt-2 mb-10">
         {type === "youtube" && (
           <iframe
             className="rounded-2xl min-h-80 w-full mt-5"
@@ -77,6 +93,16 @@ export const Card = ({ title, link, type, detail }: CardProps) => {
             </blockquote>
           </div>
         )}
+
+
+        {type === "note" && (
+          <div className="p-3 h-full border border-slate-200 rounded-lg bg-slate-300 text-sm text-gray-700">
+            <p className="break-words">{link}</p>
+          </div>
+        )}
+
+
+
       </div>
 
       {/* Footer section */}
