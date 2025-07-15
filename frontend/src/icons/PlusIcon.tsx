1d72e3290e493d94144b06579015a21be383df94
rajyadav63 <vector36razz@gmail.com>
2025-07-15 11:32:24 +0530

    camelCase

--- frontend/src/icons/PlusIcon.tsx
+++ frontend/src/icons/PlusIcon.tsx
@@ -2,9 +2,17 @@ import { iconSizeVariants, type IconsProps } from "."
 
 
 
-export const PlusIcon = (props: IconsProps)=>{
-    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-     className={iconSizeVariants[props.size]}>
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-</svg>
+export const PlusIcon = (props: IconsProps) => {
+  return <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    className={iconSizeVariants[props.size]}>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 4.5v15m7.5-7.5h-15" />
+  </svg>
 }
