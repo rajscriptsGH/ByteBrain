adf42258eb6cac9f5e84a7aafba94193f797bfce
rajyadav63 <vector36razz@gmail.com>
2025-07-17 20:04:36 +0530

    input component added

---
+++ frontend/src/components/Input.tsx
@@ -0,0 +1,19 @@
+
+
+interface InputProps {
+    placeholder: string;
+    reference?: any
+}
+
+export function Input({ placeholder }: InputProps) {
+    return (
+        <div>
+            <input
+                className="px-6 py-3 border rounded-lg outline-none"
+                type="text"
+                placeholder={placeholder}
+
+            />
+        </div>
+    )
+}
