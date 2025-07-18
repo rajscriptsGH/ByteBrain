78dcb254dbb71697418866a2a2e9b81595329f14
rajyadav63 <vector36razz@gmail.com>
2025-07-18 09:36:03 +0530

    more input box added

--- frontend/src/components/CreateContentModel.tsx
+++ frontend/src/components/CreateContentModel.tsx
@@ -13,15 +13,17 @@ export const CreateContentModel = ({ open, onClose }: CreateContentModelProps) =
         {open && (
             <div className="bg-slate-500/80 h-screen w-full fixed top-24 flex justify-center items-center">
 
-                <div className="flex flex-col p-4 cursor-pointer h-80 w-80 bg-slate-400 rounded-2xl">
+                <div className="flex flex-col p-4 cursor-pointer h-auto w-80 bg-slate-400 rounded-2xl">
                     {/* //input div */}
                     <div className="flex flex-col justify-center gap-8 ">
                         <div onClick={onClose} className="flex justify-end">
                             <CrossIcon size="lg" />
                         </div>
                         <div className="flex flex-col gap-5">
-                            <Input placeholder="Type" />
-                            <Input placeholder="Link" />
+                            <Input placeholder="Title*" />
+                            <Input placeholder="Link*" />
+                            <Input placeholder="Type*" />
+                            <Input placeholder="Details" />
                         </div>
                         <div className="flex justify-center rounded-2xl items-center w-full">
                             <Button variant="primary" text="Submit" />
