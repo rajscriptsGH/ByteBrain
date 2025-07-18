073f6f9fdc8a950abd11e7ab6d40da6317ca62ad
rajyadav63 <vector36razz@gmail.com>
2025-07-18 09:22:10 +0530

    Crossicon updated

--- frontend/src/icons/CrossIcon.tsx
+++ frontend/src/icons/CrossIcon.tsx
@@ -9,7 +9,9 @@ export const CrossIcon = (props: IconsProps) => {
         strokeWidth="1.5"
         stroke="currentColor"
         className={iconSizeVariants[props.size]}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+        <path
+            strokeLinecap="round"
+            strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
     </svg>
 
 }
