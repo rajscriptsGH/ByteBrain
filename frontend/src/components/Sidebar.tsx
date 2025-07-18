2e84f415a3e8ce3b7c580fac1e49cc0a21fd1c97
rajyadav63 <vector36razz@gmail.com>
2025-07-18 09:58:53 +0530

    sidebar added

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+
+
+export const Sidebar = () => {
+    return (
+        <div className="hidden lg:block sm:h-screen sm:w-72  border-r-2 border-slate-300">
+            <h1>Sidebar</h1>
+        </div>
+    )
+}
