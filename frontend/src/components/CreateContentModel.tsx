472277fef6b73cde8eaa0a3df75eef0081d996d4
rajyadav63 <vector36razz@gmail.com>
2025-07-18 10:54:08 +0530

    content model updated

--- frontend/src/components/CreateContentModel.tsx
+++ frontend/src/components/CreateContentModel.tsx
@@ -9,17 +9,17 @@ interface CreateContentModelProps {
 
 
 export const CreateContentModel = ({ open, onClose }: CreateContentModelProps) => {
-    return <div>
+    return <div className="flex justify-center items-center lg:ml-80">
         {open && (
-            <div className="bg-slate-500/80 h-screen w-full fixed top-24 flex justify-center items-center">
+            <div className="bg-slate-500/80 h-screen w-full fixed top-24 right-0 lg:left-80 flex justify-center items-center">
 
-                <div className="flex flex-col p-4 cursor-pointer h-auto w-80 bg-slate-400 rounded-2xl">
+                <div className="flex flex-col p-4 cursor-pointer h-auto w-96 bg-slate-400 rounded-2xl lg:mr-36">
                     {/* //input div */}
-                    <div className="flex flex-col justify-center gap-8 ">
-                        <div onClick={onClose} className="flex justify-end">
+                    <div className="flex flex-col justify-center items-center gap-8 relative ">
+                        <div onClick={onClose} className="flex justify-end absolute top-0 right-0">
                             <CrossIcon size="lg" />
                         </div>
-                        <div className="flex flex-col gap-5">
+                        <div className="flex flex-col mt-10 gap-5">
                             <Input placeholder="Title*" />
                             <Input placeholder="Link*" />
                             <Input placeholder="Type*" />
