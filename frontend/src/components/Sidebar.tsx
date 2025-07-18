0aee73098bb5b9b352df1117b6114ebb7a84f4b7
rajyadav63 <vector36razz@gmail.com>
2025-07-18 10:00:13 +0530

    Sidebar updated

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -3,7 +3,7 @@
 export const Sidebar = () => {
     return (
         <div className="hidden lg:block sm:h-screen sm:w-72  border-r-2 border-slate-300">
-            <h1>Sidebar</h1>
+
         </div>
     )
 }
