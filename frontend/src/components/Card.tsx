43b593544e51ba6582f0c9cc029d7044243336fc
rajyadav63 <vector36razz@gmail.com>
2025-07-18 10:54:32 +0530

    card updated

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -6,7 +6,7 @@ import { TwitterIcon } from "../icons/TwitterIcon";
 import { NoteIcon } from "../icons/NoteIcon";
 import { LinkIcon } from "../icons/LinkIcon";
 import { PlusIcon } from "../icons/PlusIcon";
-// import { NotionIcon } from "../icons/NotionIcon";
+import { NotionIcon } from "../icons/NotionIcon";
 
 interface CardProps {
   title: string;
@@ -42,7 +42,7 @@ export const Card = ({ title, link, type, detail }: CardProps) => {
     youtube: <YouTubeIcon size="lg" />,
     twitter: <TwitterIcon size="lg" />,
     note: <NoteIcon size="lg" />,
-    // notion: <NotionIcon size="lg" />,
+    notion: <NotionIcon size="lg" />,
     links: <LinkIcon size="lg" />,
     share: <ShareIcon size="lg" />,
     delete: <DeleteIcon size="lg" />,
