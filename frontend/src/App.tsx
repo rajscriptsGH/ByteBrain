20e4fd6936a2d2ff0d1b7124e83f86fb95de9592
rajyadav63 <vector36razz@gmail.com>
2025-07-19 07:47:59 +0530

    App.tsx updated with signin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import Dashboard from "./pages/Dashboard"
+import { Signup } from "./pages/Signup"
 
 const App = () => {
   return (
     <div>
+      <Signup />
       <Dashboard />
     </div>
   )
