9edee84ee40cffda80113df760b81d44047fd93e
rajyadav63 <vector36razz@gmail.com>
2025-07-20 09:28:19 +0530

    Home page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import { Signin } from "./pages/Signin"
 import { Signup } from "./pages/Signup"
+import { Home } from "./components/Home"
 
 const App = () => {
   return <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Home/>} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/dashboard" element={<Dashboard />} />
