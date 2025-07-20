5f8960ab4097982363557e49cca40aedec914fc4
rajyadav63 <vector36razz@gmail.com>
2025-07-20 09:50:35 +0530

    usecontent added

---
+++ frontend/src/hooks/UserContent.tsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../config";
+
+export function useContent() {
+    const [contents, setContents] = useState([])
+
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/api/v1/contnent`, {
+            headers: {
+                'Authorization': localStorage.getItem("token")
+            }
+        })
+            .then((response) => {
+                setContents(response.data.components)
+            })
+    }, [])
+
+    return contents;
+}
