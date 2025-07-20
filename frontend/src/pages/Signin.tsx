c8e144bf41242799355ac2865a84827709c6b7e4
rajyadav63 <vector36razz@gmail.com>
2025-07-20 17:59:19 +0530

    signin done

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -16,6 +16,15 @@ export const Signin = () => {
         const username = usernameRef.current?.value
         const password = passwordRef.current?.value
 
+        if (!username || !password) {
+            alert('Please enter username and password')
+            return
+        }
+
+        const token = localStorage.getItem("token");
+        if (token) {
+            navigate("/dashboard");
+        }
         try {
             const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
                 username,
@@ -28,15 +37,15 @@ export const Signin = () => {
             console.error(err);
         }
     }
-    const token = localStorage.getItem("token");
-    if (token) {
-        navigate("/dashboard");
-    }
+
 
 
     return <div className="h-screen w-full flex justify-center items-center">
         <div className="relative flex flex-col items-center justify-center rounded-xl bg-slate-300 h-96 w-80">
-            <div className="absolute top-2 right-3 ">
+            <h1 className="text-2xl font-medium absolute top-4 left-10">Signin</h1>
+            <div
+                onClick={() => navigate("/")}
+                className="absolute top-2 right-3 cursor-pointer ">
                 <CrossIcon size="lg" />
             </div>
             <div className="flex flex-col justify-center gap-5 mb-2">
@@ -46,7 +55,9 @@ export const Signin = () => {
             </div>
             <p>
                 Don't have an account
-                <span className="text-blue-600 cursor-pointer ml-2">Signup</span></p>
+                <span
+                    onClick={() => navigate('/signup')}
+                    className="text-blue-600 cursor-pointer ml-2">Signup</span></p>
         </div>
     </div>;
 };
