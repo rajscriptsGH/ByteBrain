29d0f9de71ce7dad62cc44d94b23e0232b59dd9c
rajyadav63 <vector36razz@gmail.com>
2025-07-20 15:31:26 +0530

    signin token

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -4,12 +4,14 @@ import { Input } from "../components/Input";
 import { CrossIcon } from "../icons/CrossIcon";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
+import { useNavigate } from "react-router-dom";
 
 
 export const Signin = () => {
     const usernameRef = useRef<HTMLInputElement>(null)
     const passwordRef = useRef<HTMLInputElement>(null)
 
+    const navigate = useNavigate()
     async function signin() {
         const username = usernameRef.current?.value
         const password = passwordRef.current?.value
@@ -26,7 +28,11 @@ export const Signin = () => {
             console.error(err);
         }
     }
-    window.location.href = "/dashboard";
+    const token = localStorage.getItem("token");
+    if (token) {
+        navigate("/dashboard");
+    }
+
 
     return <div className="h-screen w-full flex justify-center items-center">
         <div className="relative flex flex-col items-center justify-center rounded-xl bg-slate-300 h-96 w-80">
