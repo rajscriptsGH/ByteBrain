2a241a4add9aa6f68c09d400c8c333a0469920ae
rajyadav63 <vector36razz@gmail.com>
2025-08-31 17:36:06 +0530

    bg color and text color change for visibility

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "./Navbar";
 
 export const Home = () => {
     return (
-        <div className="bg-blue-300 min-h-screen w-full flex flex-col">
+        <div className="bg-slate-900 text-white min-h-screen w-full flex flex-col">
             <div>
                 <Navbar />
             </div>
@@ -12,10 +12,10 @@ export const Home = () => {
                 <h1 className="text-5xl lg:text-6xl font-bold text-center mt-5">
                     Welcome to <span className="text-orange-600">ByteBrain</span>
                 </h1>
-                <p className="text-2xl mt-6 font-medium text-center text-gray-800">
+                <p className="text-2xl mt-6 font-medium text-center text-gray-300">
                     Your Second Brain for a Smarter You
                 </p>
-                <p className="text-md text-gray-700 mt-6 text-center max-w-[550px] leading-relaxed">
+                <p className="text-md text-gray-400 mt-6 text-center max-w-[550px] leading-relaxed">
                     Store your thoughts, save content you care about, and organize
                     everything in one place. From creators to coders, ByteBrain gives your
                     ideas a home.
